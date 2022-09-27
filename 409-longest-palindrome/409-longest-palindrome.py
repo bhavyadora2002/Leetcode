0a2bb4bf320c7565c54d0851f106a838bf357690
bhavyadora2002 <112554752+bhavyadora2002@users.noreply.github.com>
2022-09-27 22:35:59 +0530

    Time: 148 ms (5.12%), Space: 13.8 MB (97.38%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        c=0
+        f=0
+        e=0
+        l=[]
+        if len(set(s))==1:
+            return len(s)
+        for i in s:
+            if s.count(i)%2==0 and i not in l:
+                c+=s.count(i)
+                l.append(i)
+            elif s.count(i)%2==1 and i not in l:
+                e=e+s.count(i)-1
+                f=1
+                l.append(i)
+        if f==1:
+            return c+e+1
+        return c
+        
